fd9ec01692f048bb96b8430bbca1d42ba0589c02
Lucheng_Hugh <hughqing@qq.com>
2022-01-13 17:46:35 -0600

    introduced kube and integrated with only posts+event-bus

--- event-bus/index.js
+++ event-bus/index.js
@@ -11,18 +11,18 @@ app.post("/events", (req, res, next) => {
   const event = req.body;
   events.push(event);
   console.log(event);
-  axios.post("http://localhost:4000/events", event).catch((err) => {
+  axios.post("http://posts-clusterip-srv:4000/events", event).catch((err) => {
     console.log("4000 does not respond");
   });
-  axios.post("http://localhost:4001/events", event).catch((err) => {
-    console.log("4001 does not respond");
-  });
-  axios.post("http://localhost:4002/events", event).catch((err) => {
-    console.log("4002 does not respond");
-  });
-  axios.post("http://localhost:4003/events", event).catch((err) => {
-    console.log("4003 does not respond");
-  });
+  // axios.post("http://localhost:4001/events", event).catch((err) => {
+  //   console.log("4001 does not respond");
+  // });
+  // axios.post("http://localhost:4002/events", event).catch((err) => {
+  //   console.log("4002 does not respond");
+  // });
+  // axios.post("http://localhost:4003/events", event).catch((err) => {
+  //   console.log("4003 does not respond");
+  // });
   res.send({ status: "OK" });
 });
 
